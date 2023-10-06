3004fc330915250442d723c198cb6f3f79a37a81
Junxin Zheng - Gin <junxin.zheng0001@stud.hkr.se>
2023-10-06 11:21:30 +0200

    style: :art: Prettier Configuration
    Creates the .prettierrc file for prettier configuration.

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>HKIF-APP</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-});
+})
