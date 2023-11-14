03058017a2f671a8fa34c4847cf81146ad75a061
Murwan Eisa <eisa.murwan@gmail.com>
2023-11-14 21:57:35 +0100

    feat: :lipstick: addig the user and the menu icons to the naviagtion header

--- Utilities/Navigation/StackNav.jsx
+++ Utilities/Navigation/StackNav.jsx
@@ -8,9 +8,45 @@ import LoginForm from '../../Screens/LoginForm'
 import Register from '../../Screens/Register'
 import { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
+import { Text, View } from 'react-native'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { useTheme } from '../../Styles/theme'
 
 const AuthStack = createStackNavigator()
 const AppStack = createStackNavigator()
+
+const hamburgerMenu = ({ navigation }) => {
+  const { theme } = useTheme()
+  return (
+    <View
+      style={{
+        marginLeft: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Ionicons name='menu' size={38} color={theme.colors.text} />
+    </View>
+  )
+}
+
+const profile = ({ navigation }) => {
+  const { theme } = useTheme()
+  return (
+    <View
+      style={{
+        marginRight: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <FontAwesome5 name='user-circle' size={38} color={theme.colors.text} />
+    </View>
+  )
+}
 
 const AuthStackScreen = () => (
   <AuthStack.Navigator initialRouteName='Welcome'>
@@ -29,15 +65,31 @@ const AuthStackScreen = () => (
   </AuthStack.Navigator>
 )
 
-const AppStackScreen = () => (
-  <AppStack.Navigator>
-    <AppStack.Screen name='Home' component={Home} />
-    <AppStack.Screen name='Details' component={Details} />
-  </AppStack.Navigator>
-)
+const AppStackScreen = () => {
+  const { theme } = useTheme() // Use the theme hook here
+
+  return (
+    <AppStack.Navigator
+      screenOptions={{
+        headerLeft: hamburgerMenu,
+        headerRight: profile,
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: theme.colors.accent, // Use the theme for styling
+        },
+        headerTitleStyle: {
+          color: theme.colors.text,
+        },
+      }}
+    >
+      <AppStack.Screen name='Home' component={Home} />
+      <AppStack.Screen name='Details' component={Details} />
+    </AppStack.Navigator>
+  )
+}
 
 export default function StackNav() {
-  const [isAuthenticated, setIsAuthenticated] = useState('fgsdfgd')
+  const [isAuthenticated, setIsAuthenticated] = useState('user')
   return (
     <NavigationContainer>
       {isAuthenticated ? <AppStackScreen /> : <AuthStackScreen />}
