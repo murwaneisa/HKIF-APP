5ab2176102e923727c1ff1c0b8435dc363bed990
ahmedmohd957 <ahmed.mohd957@gmail.com>
2023-11-24 22:55:14 +0100

    feat: implement bottom sheet

--- Screens/Activity.jsx
+++ Screens/Activity.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View, Text, StyleSheet, Image, Platform } from 'react-native'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import { useTheme } from '../Styles/theme'
-import { ScrollView } from 'react-native-gesture-handler'
 import Swiper from 'react-native-swiper'
 import Calendar from '../Components/Calendar'
+import BottomSheet from 'react-native-simple-bottom-sheet'
 
 function Activity({ navigation }) {
   const { theme } = useTheme()
@@ -24,80 +24,98 @@ function Activity({ navigation }) {
   }
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require('../Assets/images/movie.jpg')}
-          resizeMode='cover'
-        />
-      </View>
-      <View style={styles.nextSessionCard}>
-        <Text style={styles.nextSessionCardTitle}>
-          NEXT SESSION: 16:00 - 18:00
-        </Text>
-        <View style={styles.nextSessionCardFlex}>
-          <View style={styles.profileImageContainer}>
-            <Image
-              style={styles.profileImage}
-              source={require('../Assets/images/movie.jpg')}
-              resizeMode='cover'
-            />
-          </View>
-          <View style={styles.nextSessionCardContent}>
-            <Text style={styles.nextSessionCardContentTitle}>
-              Thursday, September 28th
-            </Text>
-            <Text style={styles.nextSessionCardContentSubtitle}>
-              Coach: XXXX | Location: The Gym{' '}
-            </Text>
+    <View style={styles.mainContainer}>
+      <ScrollView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={require('../Assets/images/movie.jpg')}
+            resizeMode='cover'
+          />
+        </View>
+        <View style={styles.nextSessionCard}>
+          <Text style={styles.nextSessionCardTitle}>
+            NEXT SESSION: 16:00 - 18:00
+          </Text>
+          <View style={styles.nextSessionCardFlex}>
+            <View style={styles.profileImageContainer}>
+              <Image
+                style={styles.profileImage}
+                source={require('../Assets/images/movie.jpg')}
+                resizeMode='cover'
+              />
+            </View>
+            <View style={styles.nextSessionCardContent}>
+              <Text style={styles.nextSessionCardContentTitle}>
+                Thursday, September 28th
+              </Text>
+              <Text style={styles.nextSessionCardContentSubtitle}>
+                Coach: XXXX | Location: The Gym
+              </Text>
+            </View>
           </View>
         </View>
-      </View>
 
-      <View style={styles.scheduleContainer}>
-        <Swiper
-          style={styles.swiper}
-          paginationStyle={styles.pagination}
-          activeDotColor={theme.colors.primary}
-        >
-          {/* Schedule List 1 */}
-          <View style={styles.slide}>
-            <Text style={styles.scheduleTitle}>
-              Schedule - {formatDate(new Date())}
-            </Text>
-            <Calendar startDate={new Date()} />
-          </View>
+        <View style={styles.scheduleContainer}>
+          <Swiper
+            style={styles.swiper}
+            paginationStyle={styles.pagination}
+            activeDotColor={theme.colors.primary}
+          >
+            {/* Schedule List 1 */}
+            <View style={styles.slide}>
+              <Text style={styles.scheduleTitle}>
+                Schedule - {formatDate(new Date())}
+              </Text>
+              <Calendar startDate={new Date()} />
+            </View>
 
-          {/* Schedule List 2 */}
-          <View style={styles.slide}>
-            <Text style={styles.scheduleTitle}>
-              Schedule -{' '}
-              {formatDate(
-                new Date(new Date().setDate(new Date().getDate() + 7))
-              )}
-            </Text>
-            <Calendar
-              startDate={new Date(new Date().setDate(new Date().getDate() + 7))}
-            />
-          </View>
-        </Swiper>
-      </View>
+            {/* Schedule List 2 */}
+            <View style={styles.slide}>
+              <Text style={styles.scheduleTitle}>
+                Schedule -{' '}
+                {formatDate(
+                  new Date(new Date().setDate(new Date().getDate() + 7))
+                )}
+              </Text>
+              <Calendar
+                startDate={
+                  new Date(new Date().setDate(new Date().getDate() + 7))
+                }
+              />
+            </View>
+          </Swiper>
+        </View>
 
-      <View style={styles.descriptionContainer}>
-        <Text style={styles.descriptionTitle}>Description</Text>
-        <Text style={styles.descriptionText}>
-          Write a short description about the activity here. Write a short
-          description about the activity here. Write a short description about
-          the activity here.
-        </Text>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.descriptionTitle}>Description</Text>
+          <Text style={styles.descriptionText}>
+            Write a short description about the activity here. Write a short
+            description about the activity here. Write a short description about
+            the activity here.
+          </Text>
+        </View>
+      </ScrollView>
+      <View style={styles.bottomSheetContainer}>
+        <View>
+          <Text>Your content</Text>
+        </View>
+        <BottomSheet isOpen={false}>
+          <View style={{ backgroundColor: 'red', height: 600 }}></View>
+        </BottomSheet>
       </View>
-    </ScrollView>
+    </View>
   )
 }
 
 const getStyles = theme =>
   StyleSheet.create({
+    container: {
+      paddingHorizontal: 20,
+    },
+    bottomSheetContainer: {
+      flex: 1,
+    },
     scheduleContainer: {
       // flex: 1,
       // backgroundColor: 'red',
@@ -115,9 +133,6 @@ const getStyles = theme =>
       fontFamily: 'Inter-Bold',
       fontSize: 18,
       marginBottom: 0,
-    },
-    container: {
-      paddingHorizontal: 20,
     },
     imageContainer: {
       width: '100%',
@@ -150,12 +165,12 @@ const getStyles = theme =>
       width: 45,
       height: 45,
       marginRight: 10,
-      borderRadius: '100%',
+      borderRadius: 50,
     },
     profileImage: {
       width: '100%',
       height: '100%',
-      borderRadius: '100%',
+      borderRadius: 50,
     },
     nextSessionCardContent: {
       // marginTop: 2,
