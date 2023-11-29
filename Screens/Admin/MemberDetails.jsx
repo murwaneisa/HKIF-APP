63232d29588388384a31e98ff8951552138b1c58
Murwan Eisa <eisa.murwan@gmail.com>
2023-11-29 19:46:35 +0100

    feat: customize the header for the member details

--- Screens/Admin/MemberDetails.jsx
+++ Screens/Admin/MemberDetails.jsx
@@ -1,7 +1,14 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
-const MemberDetails = ({ navigation }) => {
+const MemberDetails = () => {
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    navigation.setOptions({ headerTitle: 'Member Details' })
+  }, [navigation])
+
   return (
     <View>
       <Text>MemberDetails</Text>
