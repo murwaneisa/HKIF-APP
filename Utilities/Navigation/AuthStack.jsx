734b7157810ae24cdc7749347f6a8aea4a8e3f64
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-01-21 23:18:29 +0100

    fix: splash-screen & welcome-screen file names changed, navigation header-left icon updated

--- Utilities/Navigation/AuthStack.jsx
+++ Utilities/Navigation/AuthStack.jsx
@@ -1,27 +1,35 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import Splash from '../../Screens/Splash'
 import Welcome from '../../Screens/Welcome'
-import Login from '../../Screens/Login'
 import LoginForm from '../../Screens/LoginForm'
 import Register from '../../Screens/Register'
+import HeaderLeft from '../../Components/Navigation/HeaderLeft'
 
 const AuthStack = () => {
   const AuthStack = createStackNavigator()
   return (
-    <AuthStack.Navigator initialRouteName='Welcome'>
+    <AuthStack.Navigator initialRouteName='Splash'>
       <AuthStack.Screen
-        name='Welcome'
-        component={Welcome}
+        name='Splash'
+        component={Splash}
         options={{ headerShown: false }}
       />
       <AuthStack.Screen
-        name='Login'
-        component={Login}
+        name='Welcome'
+        component={Welcome}
         options={{ headerLeft: () => null, headerShown: false }}
       />
-      <AuthStack.Screen name='Login Form' component={LoginForm} />
-      <AuthStack.Screen name='Register' component={Register} />
+      <AuthStack.Screen
+        name='Login Form'
+        component={LoginForm}
+        options={{ headerLeft: () => HeaderLeft() }}
+      />
+      <AuthStack.Screen
+        name='Register'
+        component={Register}
+        options={{ headerLeft: () => HeaderLeft() }}
+      />
     </AuthStack.Navigator>
   )
 }
